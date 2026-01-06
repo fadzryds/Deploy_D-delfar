3f1013b232fb011f60eda0d4f9e1f62b846162fd
fadzryds <fadzry1603@gmail.com>
2026-01-06 16:11:25 +0700

    Push

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,29 +18,15 @@ class User extends Authenticatable implements FilamentUser
         'name', 'email', 'password', 'role', 'foto', 'nomor_karyawan',
     ];
 
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed', 
-        ];
-    }
-
     public function customer(): HasOne
     {
         return $this->hasOne(Customer::class);
     }
 
-    /**
-     * Logic akses untuk Filament di Production
-     */
     public function canAccessPanel(Panel $panel): bool
     {
         $allowedRoles = ['admin', 'staff', 'user']; 
+        
         return in_array(strtolower($this->role), $allowedRoles);
     }
 }
